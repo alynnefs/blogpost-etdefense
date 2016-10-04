1041101d686169c02bd53c1257a04355b091180c
Leonardo Silveira <sombriks@gmail.com>
2016-10-04 02:35:41 -0300

    ultimo commit antes de separar o código

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ var stage = new PIXI.Container();
 
 var alien;
 
-PIXI.loader.add("alien.png").load(function (loader, resources) {
+var points;
 
-  alien = new PIXI.Sprite(resources["alien.png"].texture);
+PIXI.loader.add("alien.json").load(function (loader, resources) {
+
+  var pts = 0;
+  points = new PIXI.Text("" + pts, { fill: "white" });
+  points.x = 10;
+  points.y = 600;
+  stage.addChild(points);
+
+  var frames = []
+  frames.push(PIXI.Texture.fromFrame("alien_down1"));
+  frames.push(PIXI.Texture.fromFrame("alien_down0"));
+  frames.push(PIXI.Texture.fromFrame("alien_down1"));
+  frames.push(PIXI.Texture.fromFrame("alien_down2"));
+  alien = new PIXI.MovieClip(frames);
   alien.interactive = true;
+  alien.animationSpeed = 0.1;
+  alien.play()
   alien.on("mousedown", function () {
     alien.y = -50;
     alien.x = Math.ceil(Math.random() * 480);
+    pts++;
+    points.text = pts;
   });
   stage.addChild(alien);
 
@@ -22,9 +39,9 @@ PIXI.loader.add("alien.png").load(function (loader, resources) {
 
 function anim() {
   renderer.render(stage);
-  if(alien.y > 640){
+  if (alien.y > 640) {
     alert("Game over!");
-  }else {
+  } else {
     alien.y++;
     requestAnimationFrame(anim);
   }
